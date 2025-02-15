279ce7ae1644fce5b80095b359dfcbc8c5b2299f
jebaseelan thiyagarajan <jebaseelan.thiyagarajan@coherent.in>
2025-02-15 20:42:58 +0530

    V 0.0.35

--- android/src/main/java/io/github/coherent/sound_generator/SoundGeneratorPlugin.java
+++ android/src/main/java/io/github/coherent/sound_generator/SoundGeneratorPlugin.java
@@ -8,7 +8,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.github.coherent.sound_generator.handlers.getOneCycleDataHandler;
 import io.github.coherent.sound_generator.handlers.isPlayingStreamHandler;
 import io.github.coherent.sound_generator.handlers.isPlayingStreamNoiseHandler;
@@ -21,97 +20,176 @@ public class SoundGeneratorPlugin implements FlutterPlugin, MethodCallHandler {
   /// when the Flutter Engine is detached from the Activity
   private SoundGenerator soundGenerator = new SoundGenerator();
   private MethodChannel channel;
+  private EventChannel onChangeIsPlaying;
+  private EventChannel onOneCycleDataHandler;
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
 
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "sound_generator");
-    channel.setMethodCallHandler(this);
-    final EventChannel onChangeIsPlaying = new EventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), isPlayingStreamHandler.NATIVE_CHANNEL_EVENT);
+    channel = new MethodChannel(binding.getBinaryMessenger(), "sound_generator");
+    channel.setMethodCallHandler(new SoundGeneratorPlugin());
+
+    // Initialize the event channels
+    onChangeIsPlaying = new EventChannel(binding.getBinaryMessenger(), isPlayingStreamHandler.NATIVE_CHANNEL_EVENT);
     onChangeIsPlaying.setStreamHandler(new isPlayingStreamHandler());
-    final EventChannel onOneCycleDataHandler = new EventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), getOneCycleDataHandler.NATIVE_CHANNEL_EVENT);
+
+    onOneCycleDataHandler = new EventChannel(binding.getBinaryMessenger(), getOneCycleDataHandler.NATIVE_CHANNEL_EVENT);
     onOneCycleDataHandler.setStreamHandler(new getOneCycleDataHandler());
 
-
-    final EventChannel onChangeIsPlayingNoise = new EventChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), isPlayingStreamNoiseHandler.NATIVE_CHANNEL_EVENT);
+    final EventChannel onChangeIsPlayingNoise = new EventChannel(binding.getBinaryMessenger(), isPlayingStreamNoiseHandler.NATIVE_CHANNEL_EVENT);
     onChangeIsPlayingNoise.setStreamHandler(new isPlayingStreamNoiseHandler());
   }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "sound_generator");
-    channel.setMethodCallHandler(new SoundGeneratorPlugin());
-    final EventChannel onChangeIsPlaying = new EventChannel(registrar.messenger(), isPlayingStreamHandler.NATIVE_CHANNEL_EVENT);
-    onChangeIsPlaying.setStreamHandler(new isPlayingStreamHandler());
-    final EventChannel onOneCycleDataHandler = new EventChannel(registrar.messenger(), getOneCycleDataHandler.NATIVE_CHANNEL_EVENT);
-    onOneCycleDataHandler.setStreamHandler(new getOneCycleDataHandler());
-  }
-
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("init")) {
-      int sampleRate = call.argument("sampleRate");
-      result.success(soundGenerator.init(sampleRate));
-    }else if (call.method.equals("release")) {
-      soundGenerator.release();
-    }else if (call.method.equals("play")) {
-      soundGenerator.startPlayback();
-    }else if (call.method.equals("play_calibration")) {
-      int frequency= call.argument("frequency");
-      int sampleRate= call.argument("sampleRate");
-      int actualVolume= call.argument("actualVolume");
-      int numSamples= call.argument("numSamples");
-      int s= call.argument("s");
-      soundGenerator.startPlayback2(frequency,  sampleRate,  actualVolume,  numSamples,  s);
-    }else if (call.method.equals("stop")) {
-      soundGenerator.stopPlayback();
-    }else if (call.method.equals("play_noise")) {
-      int frequency= call.argument("frequency");
-      int sampleRate= call.argument("sampleRate");
-      int actualVolume= call.argument("actualVolume");
-      int numSamples= call.argument("numSamples");
-      int s= call.argument("s");
-      soundGenerator.startPlayback3(sampleRate,  actualVolume,  numSamples,  s);
-    }else if (call.method.equals("stop_noise")) {
-      soundGenerator.stopPlayback3();
-    }else if (call.method.equals("isPlaying")) {
-      result.success(soundGenerator.isPlaying());
-    }else if (call.method.equals("setAutoUpdateOneCycleSample")) {
-      boolean autoUpdateOneCycleSample = call.argument("autoUpdateOneCycleSample");
-      soundGenerator.setAutoUpdateOneCycleSample(autoUpdateOneCycleSample);
-    }else if (call.method.equals("setFrequency")) {
-      double frequency = call.argument("frequency");
-      soundGenerator.setFrequency((float)frequency);
-    }else if (call.method.equals("setDecibel")) {
-      double decibel = call.argument("decibel");
-      soundGenerator.setDecibel((float)decibel);
-    }else if(call.method.equals("setNoiseDecibel")){
-      double noise_decibel = call.argument("noise_decibel");
-      soundGenerator.setNoiseDecibel((float)noise_decibel);
-    }else if (call.method.equals("setWaveform")) {
-      String waveType = call.argument("waveType");
-      soundGenerator.setWaveform(WaveTypes.valueOf(waveType));
-    }else if (call.method.equals("setBalance")) {
-      double balance = call.argument("balance");
-      soundGenerator.setBalance((float)balance);
-    }else if (call.method.equals("setVolume")) {
-      double volume = call.argument("volume");
-      soundGenerator.setVolume((float)volume);
-    }else if (call.method.equals("getSampleRate")) {
-      result.success(soundGenerator.getSampleRate());
-    }else if (call.method.equals("refreshOneCycleData")) {
-      soundGenerator.refreshOneCycleData();
-    }else {
-      result.notImplemented();
+    try {
+      switch (call.method) {
+        case "init": {
+          Integer sampleRate = call.argument("sampleRate");
+          if (sampleRate == null) {
+            result.error("INVALID_ARGUMENT", "sampleRate is required", null);
+            return;
+          }
+          result.success(soundGenerator.init(sampleRate));
+          break;
+        }
+        case "release":
+          soundGenerator.release();
+          result.success(null);
+          break;
+        case "play":
+          soundGenerator.startPlayback();
+          result.success(null);
+          break;
+        case "play_calibration": {
+          Integer frequency = call.argument("frequency");
+          Integer sampleRate = call.argument("sampleRate");
+          Integer actualVolume = call.argument("actualVolume");
+          Integer numSamples = call.argument("numSamples");
+          Integer s = call.argument("s");
+
+          if (frequency == null || sampleRate == null || actualVolume == null || numSamples == null || s == null) {
+            result.error("INVALID_ARGUMENT", "Missing parameters for play_calibration", null);
+            return;
+          }
+          soundGenerator.startPlayback2(frequency, sampleRate, actualVolume, numSamples, s);
+          result.success(null);
+          break;
+        }
+        case "stop":
+          soundGenerator.stopPlayback();
+          result.success(null);
+          break;
+        case "play_noise": {
+          Integer sampleRate = call.argument("sampleRate");
+          Integer actualVolume = call.argument("actualVolume");
+          Integer numSamples = call.argument("numSamples");
+          Integer s = call.argument("s");
+
+          if (sampleRate == null || actualVolume == null || numSamples == null || s == null) {
+            result.error("INVALID_ARGUMENT", "Missing parameters for play_noise", null);
+            return;
+          }
+          soundGenerator.startPlayback3(sampleRate, actualVolume, numSamples, s);
+          result.success(null);
+          break;
+        }
+        case "stop_noise":
+          soundGenerator.stopPlayback3();
+          result.success(null);
+          break;
+        case "isPlaying":
+          result.success(soundGenerator.isPlaying());
+          break;
+        case "setAutoUpdateOneCycleSample": {
+          Boolean autoUpdateOneCycleSample = call.argument("autoUpdateOneCycleSample");
+          if (autoUpdateOneCycleSample == null) {
+            result.error("INVALID_ARGUMENT", "autoUpdateOneCycleSample is required", null);
+            return;
+          }
+          soundGenerator.setAutoUpdateOneCycleSample(autoUpdateOneCycleSample);
+          result.success(null);
+          break;
+        }
+        case "setFrequency": {
+          Double frequency = call.argument("frequency");
+          if (frequency == null) {
+            result.error("INVALID_ARGUMENT", "frequency is required", null);
+            return;
+          }
+          soundGenerator.setFrequency(frequency.floatValue());
+          result.success(null);
+          break;
+        }
+        case "setDecibel": {
+          Double decibel = call.argument("decibel");
+          if (decibel == null) {
+            result.error("INVALID_ARGUMENT", "decibel is required", null);
+            return;
+          }
+          soundGenerator.setDecibel(decibel.floatValue());
+          result.success(null);
+          break;
+        }
+        case "setNoiseDecibel": {
+          Double noiseDecibel = call.argument("noise_decibel");
+          if (noiseDecibel == null) {
+            result.error("INVALID_ARGUMENT", "noise_decibel is required", null);
+            return;
+          }
+          soundGenerator.setNoiseDecibel(noiseDecibel.floatValue());
+          result.success(null);
+          break;
+        }
+        case "setWaveform": {
+          String waveType = call.argument("waveType");
+          if (waveType == null) {
+            result.error("INVALID_ARGUMENT", "waveType is required", null);
+            return;
+          }
+          try {
+            soundGenerator.setWaveform(WaveTypes.valueOf(waveType));
+          } catch (IllegalArgumentException e) {
+            result.error("INVALID_WAVEFORM", "Invalid waveform type: " + waveType, null);
+            return;
+          }
+          result.success(null);
+          break;
+        }
+        case "setBalance": {
+          Double balance = call.argument("balance");
+          if (balance == null) {
+            result.error("INVALID_ARGUMENT", "balance is required", null);
+            return;
+          }
+          soundGenerator.setBalance(balance.floatValue());
+          result.success(null);
+          break;
+        }
+        case "setVolume": {
+          Double volume = call.argument("volume");
+          if (volume == null) {
+            result.error("INVALID_ARGUMENT", "volume is required", null);
+            return;
+          }
+          soundGenerator.setVolume(volume.floatValue());
+          result.success(null);
+          break;
+        }
+        case "getSampleRate":
+          result.success(soundGenerator.getSampleRate());
+          break;
+        case "refreshOneCycleData":
+          soundGenerator.refreshOneCycleData();
+          result.success(null);
+          break;
+        default:
+          result.notImplemented();
+          break;
+      }
+    } catch (Exception e) {
+      result.error("UNKNOWN_ERROR", e.getMessage(), null);
     }
   }
+
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
